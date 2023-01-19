d7044138f983be6e63579135821395fa98f37ed2
EnderDE77 <erliskendezi@gmail.com>
2023-01-19 23:29:58 +0100

    git commit

--- src/main/java/codes/library/visual/Selling.java
+++ src/main/java/codes/library/visual/Selling.java
@@ -1,5 +1,7 @@
 package codes.library.visual;
 
 public abstract class Selling {
-    //TODO: Install a window that manages to connect with WHE and Warehouse by removing or adding books, p sure entering is similar
+    /*TODO: Install a window that manages to connect with WHE and Warehouse by removing or adding books, p sure Entering is similar
+    oh yea make this return a Stage that we can use in Main
+     */
 }
